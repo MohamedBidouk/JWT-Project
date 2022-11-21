220d8e7e368da67404cee3129f77b1ef1b6b2e68
MohamedBidouk <92817844+MohamedBidouk@users.noreply.github.com>
2022-11-21 17:56:58 +0100

    first commit

---
+++ src/main/java/com/mohamed/users/service/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.mohamed.users.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mohamed.users.entities.Role;
+import com.mohamed.users.entities.User;
+import com.mohamed.users.repos.RoleRepository;
+import com.mohamed.users.repos.UserRepository;
+
+@Transactional
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	RoleRepository roleRepository;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Override
+	public User saveUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		return userRepository.save(user);
+	}
+
+	@Override
+	public User findUserByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+
+	@Override
+	public Role addRole(Role role) {
+		return roleRepository.save(role);
+	}
+
+	@Override
+	public User addRoleToUser(String username, String rolename) {
+		User usr = userRepository.findByUsername(username);
+		Role role = roleRepository.findByRole(rolename);
+		
+		usr.getRoles().add(role);
+		
+		//userRepository.save(usr); transactional will do that
+		
+		return usr;
+	}
+
+	@Override
+	public List<User> findAllUsers() {
+		return userRepository.findAll();
+	}
+
+}
