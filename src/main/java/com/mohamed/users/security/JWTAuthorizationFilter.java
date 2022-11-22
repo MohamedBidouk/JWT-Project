11af914349c525e03cb71af2c0116659ef2a44a1
MohamedBidouk <92817844+MohamedBidouk@users.noreply.github.com>
2022-11-22 16:10:30 +0100

    Fini

--- src/main/java/com/mohamed/users/security/JWTAuthorizationFilter.java
+++ src/main/java/com/mohamed/users/security/JWTAuthorizationFilter.java
@@ -29,6 +29,17 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter{
 									FilterChain filterChain)
 			throws ServletException, IOException {
 		
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods",
+		"GET,HEAD,OPTIONS,POST,PUT,DELETE");
+		response.addHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+		response.addHeader("Access-Control-Expose-Headers","Authorization, Access-ControlAllow-Origin,Access-Control-Allow-Credentials ");
+		if (request.getMethod().equals("OPTIONS"))
+		{
+		response.setStatus(HttpServletResponse.SC_OK);
+		return;
+		}
+		
 		String jwt = request.getHeader("Authorization");
 		
 		if (jwt == null  || !jwt.startsWith(SecParams.PREFIX)) {
